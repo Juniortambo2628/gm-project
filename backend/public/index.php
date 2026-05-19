014cb47a76547b59b6269fe41629eb40ba3d151d
Juniortambo2628 <100776648+Juniortambo2628@users.noreply.github.com>
2026-05-19 10:45:21 +0300

    chore: make index.php relative and migration proof for cPanel

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,7 +7,7 @@ define('LARAVEL_START', microtime(true));
 
 // Dynamic Paths for Local Development vs Production cPanel Environment
 $isLocal = file_exists(__DIR__.'/../vendor/autoload.php');
-$corePath = $isLocal ? __DIR__.'/..' : '/home/zhpebukm/gmconsulting-core';
+$corePath = $isLocal ? __DIR__.'/..' : __DIR__.'/../../gmconsulting-core';
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = $corePath.'/storage/framework/maintenance.php')) {
